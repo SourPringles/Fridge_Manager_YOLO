5f7ec2745a6feb67e536e3db7937d81cac5fcd12
KJM <chance0425@naver.com>
2025-04-04 13:41:20 +0900

    Refactor Image Processing Module Path

---
+++ imageProcesser/qrDetection_cv2.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+
+def detect_qr_codes_cv2(image):
+    """
+    QR코드를 인식하고 좌표를 반환하는 함수
+    - image: OpenCV 이미지 객체
+    - 반환값: QR코드의 데이터와 좌표를 포함하는 딕셔너리
+    """
+
+    return None
