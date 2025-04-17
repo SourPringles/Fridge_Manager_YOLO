d25cc34590899b12380f5597dedf63ad317fd329
KJM <chance0425@naver.com>
2025-04-17 14:06:42 +0900

    batch 파일 수정

--- Run Server.bat
+++ Run Server.bat
@@ -1,15 +1,20 @@
 @echo off
-echo ===================================
-echo Starting AI Item Manager API Server
-echo ===================================
+echo =========================================================
+echo            Starting AI Item Manager API Server
+echo =========================================================
 
 :: 가상환경 활성화
+echo Activating Virtual environment...
+echo =========================================================
 call .venv\Scripts\activate.bat
 
 :: 활성화 확인
 echo Virtual environment activated
+echo =========================================================
 
 :: 서버 실행
+echo Initializing API server... This may take a few seconds.
+echo =========================================================
 python AIMA.py
 
 :: 혹시 오류로 종료되었을 때 바로 닫히지 않도록
